c03e62ee601be6f3504e14bb08b99fbca35b2e46
Mohamed Fariz Nazeer <faariz@gmail.com>
2021-08-25 14:50:56 +0800

    Update update_track comment in variables
    Co-authored-by: Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -272,7 +272,7 @@ variable "maintenance_window" {
   description = <<-EOT
   day_utc: The day of the week (1-7) in UTC timezone - starting from Monday.
   hour_utc: The hour of the day (0-23) in UTC timezone - ignored if day is not set.
-  update_track: The update track of maintenance window. Can be either `canary` or `stable`.
+  update_track: The update track of maintenance window - can be either `canary` or `stable`.
   EOT
   type = object({
     day_utc      = number
